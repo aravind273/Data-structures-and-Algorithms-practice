501b452a3a04b000ce0e1ea7bb917c8c0f0911d0
Aravind Eramalla <42810684+aravind273@users.noreply.github.com>
2021-06-17 14:03:59 +0530

    Update SelectionSort.cpp

--- Sorting algorithms/SelectionSort.cpp
+++ Sorting algorithms/SelectionSort.cpp
@@ -1,5 +1,8 @@
 void selectionSort(int *arr, int n)
 {
+    // traverse i+1 to n elements and find the minelement and swap it with current element 
+    // After each iteration the min element is fixed 
+    //O(n^2)
     for(int i=0;i<n;i++)
     {
         int minval=arr[i];
