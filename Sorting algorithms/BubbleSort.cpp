54d286b84ae153d0dcbcd5c56472971c4133f142
Aravind Eramalla <42810684+aravind273@users.noreply.github.com>
2021-06-17 14:02:12 +0530

    Update BubbleSort.cpp

--- Sorting algorithms/BubbleSort.cpp
+++ Sorting algorithms/BubbleSort.cpp
@@ -1,6 +1,8 @@
 void bubbleSort(int *arr, int n)
 {
     //Write your code 
+    // swap consective elements after each iteration the max element(last pos) is fixed 
+    //O(n^2)
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n-i;j++)
