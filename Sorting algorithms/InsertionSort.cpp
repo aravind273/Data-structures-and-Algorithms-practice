4d885123af36cd4eb8b4c99faed0b61f6747dd31
Aravind Eramalla <42810684+aravind273@users.noreply.github.com>
2022-05-11 21:51:55 +0530

    Update InsertionSort.cpp
    Adding more explaination

--- Sorting algorithms/InsertionSort.cpp
+++ Sorting algorithms/InsertionSort.cpp
@@ -5,6 +5,14 @@ void insertionSort(int *arr, int n)
     // sorted is left field and unsorted is right field and we will pick one value from unsorted and compare with sorted values and we 
     // will place the unsorted value at correct position in sorted array.
     
+//                 5 4 3 
+//  first pass :   j i
+//                 4 5 3
+//  second pass :    j i
+//                 4 3 5 
+//                 j   i
+//                 3 4 5  
+    
     for(int i=1;i<n;i++)
     {
         int j=i-1;
